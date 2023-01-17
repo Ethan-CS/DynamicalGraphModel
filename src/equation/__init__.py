4f469637151ccbe8245b002c6aa3c11cd611c295
ethanhunter <e.hunter.2@research.gla.ac.uk>
2023-01-17 17:42:43 +0000

    Removed debugging print statements

--- src/equation/__init__.py
+++ src/equation/__init__.py
@@ -5,11 +5,11 @@ import networkx as nx
 import numpy as np
 import sympy as sym
 
-from equation import generation, closing
+from equation import generation, closing, measure_runtimes
 from equation.Term import Term, Vertex
+from equation_MC_comparison import measure_runtimes
 from equation.generation import get_single_equations, generate_equations
 from equation.solving import initial_conditions, solve_equations, solve
-from equation_MC_comparison import measure_generation_runtimes, measure_mcmc_runtimes
 from model_params.cmodel import get_SIR, CModel
 
 SYS_STDOUT = sys.stdout
@@ -23,37 +23,10 @@ def main():
     for p in np.linspace(0.01, 0.2, 20):
         p = round(p, 2)
         print(f'\n *** p={p} ***')
+        print(f'\n - Monte Carlo -')
         measure_runtimes('random', v, iterations, p, t_max, 'mc', timeout)
+        print(f'\n - Equations -')
         measure_runtimes('random', v, iterations, p, t_max, 'equations', timeout)
-
-
-def measure_runtimes(graph_type, num_vertices, iterations, p, t_max, method, timeout):
-    sir_model = CModel.make_SIR(0.5, 0.1)
-    with open(f'../data/{graph_type}_{method.replace(" ", "_")}_data.csv', 'w+') as f:
-        sys.stdout = f  # Change the standard output to the file we created.
-        print(f'number of vertices,{"p," if p != 0 else ""}time to solve')
-
-        for i in range(2, num_vertices+1):
-            sys.stdout = SYS_STDOUT
-            print(f'\nnumber of vertices: {i}')
-
-            # Get the specified graph
-            g = None
-            if graph_type == 'path':
-                g = nx.path_graph(i)
-            elif graph_type == 'cycle':
-                g = nx.cycle_graph(i)
-            elif graph_type == 'random':
-                g = nx.erdos_renyi_graph(i, p)
-            print(f'params I got: graph={graph_type}, num_vertices={num_vertices} p={p}, method={method}')
-
-            # Create (and solve) the model
-            if method == 'eq' or method == 'equations':
-                print('Doing equations')
-                measure_generation_runtimes(g, iterations, sir_model, timeout, f, True, True, t_max)
-            elif method == 'mc' or method == 'mcmc' or method == 'monte carlo':
-                print('Doing MCMC')
-                measure_mcmc_runtimes(g, p, iterations, sir_model, timeout, f, t_max)
 
 
 def runtime_large_model():
